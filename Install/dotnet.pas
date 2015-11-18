109e65c5fb01d46cdbd739f07f8e45ec78f342f2
Hesoy <s.shmygol@gmail.com>
2015-11-19 00:45:36 +0200

    FIX#2

--- Install/dotnet.pas
+++ Install/dotnet.pas
@@ -1,13 +1,28 @@
-//-----------------------------------------------------------------------------
-//  Проверка наличия нужного фреймворка
-//-----------------------------------------------------------------------------
+function GetUninstallString: string;
+var
+  sUnInstPath: string;
+  sUnInstallString: String;
+begin
+  Result := '';
+  sUnInstPath := ExpandConstant('Software\Microsoft\Windows\CurrentVersion\Uninstall\{361E65A9-ABA7-44CA-A747-99B28252BA56}_is1'); //Your App GUID/ID
+  sUnInstallString := '';
+  if not RegQueryStringValue(HKLM, sUnInstPath, 'UninstallString', sUnInstallString) then
+    RegQueryStringValue(HKCU, sUnInstPath, 'UninstallString', sUnInstallString);
+  Result := sUnInstallString;
+end;
+
+function IsUpgrade: Boolean;
+begin
+  Result := (GetUninstallString() <> '');
+end;
+
 function IsDotNetDetected(version: string; release: cardinal): boolean;
 
 var 
-    reg_key: string; // Просматриваемый подраздел системного реестра
-    success: boolean; // Флаг наличия запрашиваемой версии .NET
-    release45: cardinal; // Номер релиза для версии 4.5.x
-    key_value: cardinal; // Прочитанное из реестра значение ключа
+    reg_key: string;
+    success: boolean; 
+    release45: cardinal;
+    key_value: cardinal;
     sub_key: string;
 
 begin
@@ -15,7 +30,7 @@ begin
     success := false;
     reg_key := 'SOFTWARE\Microsoft\NET Framework Setup\NDP\';
     
-    // Вресия 3.0
+
     if Pos('v3.0', version) = 1 then
       begin
           sub_key := 'v3.0';
@@ -24,7 +39,6 @@ begin
           success := success and (key_value = 1);
       end;
 
-    // Вресия 3.5
     if Pos('v3.5', version) = 1 then
       begin
           sub_key := 'v3.5';
@@ -33,7 +47,7 @@ begin
           success := success and (key_value = 1);
       end;
 
-     // Вресия 4.0 клиентский профиль
+
      if Pos('v4.0 Client Profile', version) = 1 then
       begin
           sub_key := 'v4\Client';
@@ -42,7 +56,7 @@ begin
           success := success and (key_value = 1);
       end;
 
-     // Вресия 4.0 расширенный профиль
+
      if Pos('v4.0 Full Profile', version) = 1 then
       begin
           sub_key := 'v4\Full';
@@ -51,7 +65,7 @@ begin
           success := success and (key_value = 1);
       end;
 
-     // Вресия 4.5
+
      if Pos('v4.5', version) = 1 then
       begin
           sub_key := 'v4\Full';
@@ -64,27 +78,50 @@ begin
 
 end;
 
-//-----------------------------------------------------------------------------
-//  Функция-обертка для детектирования конкретной нужной нам версии
-//-----------------------------------------------------------------------------
+
 function IsRequiredDotNetDetected(): boolean;
 begin
     result := IsDotNetDetected('v4.0 Full Profile', 0);
 end;
 
-//-----------------------------------------------------------------------------
-//    Callback-функция, вызываемая при инициализации установки
-//-----------------------------------------------------------------------------
-function InitializeSetup(): boolean;
+
+
+
+function InitializeSetup: Boolean;
+var
+  V: Integer;
+  iResultCode: Integer;
+  sUnInstallString: string;
+  ErrCode: integer;
 begin
-
-  // Если нет тербуемой версии .NET выводим сообщение о том, что инсталлятор
-  // попытается установить её на данный компьютер
-  if not IsDotNetDetected('v4.0 Full Profile', 0) then
+  Result := True; // in case when no previous version is found
+  if RegValueExists(HKEY_LOCAL_MACHINE,'Software\Microsoft\Windows\CurrentVersion\Uninstall\{361E65A9-ABA7-44CA-A747-99B28252BA56}_is1', 'UninstallString') then  //Your App GUID/ID
+  begin
+    V := MsgBox(ExpandConstant('WARNING! Old version of MemoNote is found on your computer. Do you want to uninstall it?'), mbInformation, MB_YESNO); //Custom Message if App installed
+    if V = IDYES then
     begin
-      MsgBox('{#Name} requires Microsoft .NET Framework 4.0 Full Profile.'#13#13
-             'The installer will attempt to install it', mbInformation, MB_OK);
-    end;   
-
-  result := true;
+      sUnInstallString := GetUninstallString();
+      sUnInstallString :=  RemoveQuotes(sUnInstallString);
+      Exec(ExpandConstant(sUnInstallString), '', '', SW_SHOW, ewWaitUntilTerminated, iResultCode);
+      Result := True; //if you want to proceed after uninstall
+                //Exit; //if you want to quit after uninstall
+    end
+    else
+      Result := False; //when older version present and not uninstalled
+  end;
+  if  not  IsRequiredDotNetDetected() then 
+	begin
+        V := MsgBox('MemoNote requires Microsoft .NET Framework 4.0 Client Profile.'#13#13
+            'Please install Microsoft .NET Framework 4.0 and re-install this app.'#13
+            'Do you want to go to microsoft.com and download it now?', mbInformation, MB_YESNO);
+        if V = IDYES then
+		begin
+			ShellExec('open', 'http://www.microsoft.com/en-us/download/details.aspx?id=42643','', '', SW_SHOW, ewNoWait, ErrCode);
+			result := false;
+		end
+		else
+			result := false;		
+    end 
+  else  
+    result := true;
 end;
